0d63c61bc87e96155dce929dfdb5ea81ff374743
vitforlinux <127871421+vitforlinux-gimp@users.noreply.github.com>
2024-06-07 16:27:05 +0200

    little fix

--- zahnpasta-plus-299.scm
+++ zahnpasta-plus-299.scm
@@ -1,5 +1,10 @@
 ;Modernized with ModernizeMatic8 for Gimp 2.10.28 by vitforlinux.wordpress.com - dont remove
 
+; Fix code for gimp 2.99.6 working in 2.10
+(cond ((not (defined? 'gimp-drawable-get-width)) (define gimp-drawable-get-width gimp-drawable-width)))
+(cond ((not (defined? 'gimp-drawable-get-height)) (define gimp-drawable-get-height gimp-drawable-height)))
+
+
 (cond ((not (defined? 'gimp-text-fontname)) (define (gimp-text-fontname fn1 fn2 fn3 fn4 fn5 fn6 fn7 fn8 PIXELS fn9) (gimp-text-font fn1 fn2 fn3 fn4 fn5 fn6 fn7 fn8 fn9))))
 
 		 (if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10)
