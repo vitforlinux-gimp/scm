1dca5efec1def03e31d86c526f003b0c8f2eacd7
vitforlinux <127871421+vitforlinux-gimp@users.noreply.github.com>
2024-06-08 17:04:59 +0200

    fixed for 2.99.19

--- rakugaki299.scm
+++ rakugaki299.scm
@@ -9,6 +9,10 @@
 (cond ((not (defined? 'gimp-drawable-get-width)) (define gimp-drawable-get-width gimp-drawable-width)))
 (cond ((not (defined? 'gimp-drawable-get-height)) (define gimp-drawable-get-height gimp-drawable-height)))
 
+		 (if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10)
+        (define sfbrush '("Pencil Scratch" 1.0 20 0))
+  (define sfbrush "Pencil Scratch")	)
+
 (define (script-fu-rakugaki299 image drawable randpoint brush)
 
 (let* (
@@ -25,7 +29,7 @@
        (xa 0)(ya 0))
        
 (gimp-context-push)
-  ; (gimp-image-undo-group-start image)
+   (gimp-image-undo-group-start image)
    		(gimp-context-set-paint-mode LAYER-MODE-NORMAL-LEGACY)
 		 (if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10)
         (gimp-context-set-brush (car  brush)) 
@@ -49,7 +53,7 @@
       (gimp-paintbrush-default drawable point segment)
       (set! count (+ count 1)) )
 
-   ;(gimp-image-undo-group-end image)
+   (gimp-image-undo-group-end image)
    (gimp-context-pop)
    (gimp-displays-flush) ))
 
@@ -63,7 +67,7 @@ _"Rakugaki (Graffiti) 299..."
                     SF-IMAGE      "Image"       0
                     SF-DRAWABLE   "Drawable"    0
                     SF-ADJUSTMENT "Power"  '(30 1 1000 1 10 0 1)
-                    SF-BRUSH      "Brush"            '("Pencil Scratch" 1.0 20 0) )
+                    SF-BRUSH      "Brush"            sfbrush )
 
 (script-fu-menu-register "script-fu-rakugaki299"
 _"<Image>/Script-Fu/Alchemy")
