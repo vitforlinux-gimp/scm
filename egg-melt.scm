85c31ec052f8b7a0ff7be552a0f9d01232c2865a
vitforlinux <127871421+vitforlinux-gimp@users.noreply.github.com>
2024-09-15 13:11:59 +0200

    Fixed script-fu drop shadow (other system)

--- egg-melt.scm
+++ egg-melt.scm
@@ -81,7 +81,9 @@
 ;;;;create the shadow
 (if (> shadow-size 0)
   (begin
-    (script-fu-drop-shadow image img-layer shadow-size shadow-size shadow-size '(0 0 0) shadow-opacity FALSE)
+  (if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10)
+      (script-fu-drop-shadow image img-layer shadow-size shadow-size shadow-size '(0 0 0) shadow-opacity FALSE)
+    (script-fu-drop-shadow image (vector img-layer) shadow-size shadow-size shadow-size '(0 0 0) shadow-opacity FALSE))
     (set! tmp-layer (car (gimp-layer-new image width height RGBA-IMAGE "temp" 100 LAYER-MODE-NORMAL-LEGACY)))
     (gimp-image-insert-layer image tmp-layer 0 -1)
 	(gimp-image-raise-item image tmp-layer)
