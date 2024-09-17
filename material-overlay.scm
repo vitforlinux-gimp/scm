f5f13cc945fcc9786c3da0ac7357f458b46727a1
vitforlinux <127871421+vitforlinux-gimp@users.noreply.github.com>
2024-09-17 18:04:30 +0200

    fix for 2.99.19

--- material-overlay.scm
+++ material-overlay.scm
@@ -1,15 +1,25 @@
-(define list-material-dir '("Carbon fiber" "Aluminium Brushed" "Aluminium Brushed Light" "Blue Jeans" "Dark Jeans" "Bovination 2.99" "Camouflage 2.99" "Plasma" "Patchwork" "Diffraction" "Pizza"))
+(define list-material-mo-dir '("Carbon fiber" "Aluminium Brushed" "Aluminium Brushed Light" "Blue Jeans" "Washed Jeans" "Dark Jeans" "Bovination 2.99" "Camouflage 2.99" "Plasma" "Patchwork" "Diffraction" "Pizza"))
 
+(cond ((not (defined? 'gimp-context-set-pattern-ng)) (define (gimp-context-set-pattern-ng value) 
+(if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10) 
+						 (gimp-context-set-pattern value)
+				(gimp-context-set-pattern (car (gimp-pattern-get-by-name value)))
+				))))
+(cond ((not (defined? 'gimp-context-set-gradient-ng)) (define (gimp-context-set-gradient-ng value) 
+(if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10) 
+						 (gimp-context-set-gradient value)
+				(gimp-context-set-gradient (car (gimp-gradient-get-by-name value)))
+				))))
 		
-		(define  (material-carbon fond) (begin
-				(gimp-context-set-pattern "Parque #3")
+		(define  (material-mo-carbon fond) (begin
+				(gimp-context-set-pattern-ng "Parque #3")
 				(gimp-drawable-fill fond FILL-PATTERN)
 				(gimp-drawable-desaturate fond 1)
 				(gimp-drawable-brightness-contrast fond -0.5 0.15)
 			))
 
 			
-		(define  (material-bovination-2 fond image) (begin
+		(define  (material-mo-bovination-2 fond image) (begin
 				(plug-in-solid-noise 0 image fond 0 0 (random 65535) 1 16 4)
 					(gimp-drawable-equalize fond 0)
 					;	(gimp-drawable-threshold fond HISTOGRAM-VALUE 0 1)
@@ -24,14 +34,14 @@
 				    (gimp-layer-set-lock-alpha fond TRUE)
 				(plug-in-oilify 1 image fond 20 0) 
 			))
-		(define (material-camo2 fond image) (begin
+		(define (material-mo-camo2 fond image) (begin
 					(gimp-selection-none image)
 					(define color1 '(136 125 52)) 
 					(define color2 '(62 82 22))
 					(define color3  '(50 28 0))
 					;(gimp-context-set-paint-mode LAYER-MODE-NORMAL-LEGACY)
 					(gimp-context-set-paint-mode LAYER-MODE-NORMAL)
-				(material-bovination-2 fond image)
+				(material-mo-bovination-2 fond image)
 				(gimp-image-select-color image 0 fond '(255 255 255) )
 				(gimp-selection-grow image 2)
 				(gimp-context-set-foreground color1 )
@@ -42,7 +52,7 @@
 				(gimp-selection-none image)
 				(gimp-image-select-color image 0 fond  color1)
 				 (gimp-selection-grow image 2)
-				(material-bovination-2 fond image)
+				(material-mo-bovination-2 fond image)
 				(gimp-selection-none image)
 				(gimp-image-select-color image 0 fond '(0 0 0) )
 				(gimp-selection-grow image 2)
@@ -59,8 +69,8 @@
 				(plug-in-oilify 1 image fond 20 0) 
 			))
 
-		(define  (material-alubrushed fond image) (begin
-				(gimp-context-set-pattern "Blue Squares")
+		(define  (material-mo-alubrushed fond image) (begin
+				(gimp-context-set-pattern-ng "Blue Squares")
 				(gimp-drawable-fill fond FILL-PATTERN)
 				(gimp-drawable-desaturate fond 1)
 				(gimp-drawable-brightness-contrast fond -0.5 0.15)
@@ -71,9 +81,12 @@
 				(gimp-drawable-colorize-hsl fond 0 0 10 )
 				;(script-fu-colorize 0 image fond 959595 1)
 			))
-		(define  (material-alubrushed-light fond image) (begin
+		(define  (material-mo-alubrushed-light fond image) (begin
 				;(gimp-context-set-gradient "Flare Rays Size 1")
-				(gimp-context-set-gradient "Rounded edge")
+				(if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10) 
+						 (gimp-context-set-gradient "Rounded edge")
+				(gimp-context-set-gradient (car (gimp-gradient-get-by-name "Rounded Edge")))
+				)
 				(gimp-context-set-gradient-repeat-mode 0)
 				(gimp-drawable-edit-gradient-fill 
 			fond
@@ -85,7 +98,7 @@
 			;REPEAT-NONE
 			;FALSE
 			;FALSE
-			100
+			1
 			0
 			0 ;FALSE
 			0
@@ -103,9 +116,13 @@
 				(gimp-drawable-colorize-hsl fond 0 0 10 )
 			))
 			
-					(define  (material-blue-jeans fond image col-opt) (begin
-				(gimp-context-set-gradient "Rounded edge")
-				(gimp-context-set-gradient-repeat-mode 1)
+					(define  (material-mo-blue-jeans fond image col-opt) (begin
+			;	(gimp-context-set-gradient-ng "Rounded edge")
+(if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10) 
+						 (gimp-context-set-gradient "Rounded edge")
+				(gimp-context-set-gradient (car (gimp-gradient-get-by-name "Rounded Edge")))
+				)
+				(gimp-context-set-gradient-repeat-mode 2)
 				(gimp-drawable-edit-gradient-fill 
 			fond
 			;BLEND-FG-TRANSPARENT
@@ -116,7 +133,7 @@
 			;REPEAT-NONE
 			;FALSE
 			;FALSE
-			100
+			1
 			0
 			0 ;FALSE
 			0
@@ -129,32 +146,39 @@
 				(gimp-drawable-desaturate fond 1)
 				(plug-in-gauss-iir TRUE image fond 3 TRUE TRUE)
 				;(gimp-drawable-colorize-hsl fond 230 36 20 )
-				
+
 																(if (= col-opt 0) 
-				(gimp-drawable-colorize-hsl fond 230 36 20 )
-				;(gimp-drawable-brightness-contrast fond 0.0 -0.5)
+				(begin (gimp-drawable-colorize-hsl fond 230 36 20 )
+				(gimp-drawable-brightness-contrast fond -0.1 0.3))
+			
+		)
+				
+																(if (= col-opt 1) 
+				(begin(gimp-drawable-colorize-hsl fond 230 36 20 )
+				(gimp-drawable-brightness-contrast fond -0.1 0.1))
 			
 		)
 		
-																(if (= col-opt 1) 
-				;(gimp-drawable-colorize-hsl fond 0 0 0)
-				(gimp-drawable-brightness-contrast fond -0.2 0.4)
+																(if (= col-opt 2) 
+				(begin (gimp-drawable-colorize-hsl fond 10 10 10)
+				(gimp-drawable-brightness-contrast fond -0.1 0.3)
+				(gimp-drawable-desaturate fond 1))
 			
 		)
 			))
 			
-					(define (material-plasma fond image)  			(begin
+					(define (material-mo-plasma fond image)  			(begin
           ;(gimp-layer-set-lock-alpha fond TRUE)
              (plug-in-plasma 1 image fond (rand 999999999) 7) ; Add plasma
 			))
-								(define (material-patchwork fond image)  			(begin
+								(define (material-mo-patchwork fond image)  			(begin
              (plug-in-plasma 1 image fond (rand 999999999) 7) ; Add plasma
 	                   (plug-in-cubism 1 image fond 6 10 0)			                   
 			))
-				(define (material-pizza fond image)  			(begin
+				(define (material-mo-pizza fond image)  			(begin
           ;(gimp-layer-set-lock-alpha fond TRUE)
             
-             (material-bovination-2 fond image)
+             (material-mo-bovination-2 fond image)
 	     (gimp-selection-none image)
 	     (gimp-image-select-color image 0 fond '(0 0 0) )
 	     (gimp-selection-grow image 3)
@@ -165,14 +189,14 @@
 	      (plug-in-oilify 1 image fond 20 0) 
 			))
 			
-		(define (material-diffraction fond image)  			
+		(define (material-mo-diffraction fond image)  			
         (begin  
               (set! *seed* (car (gettimeofday))) ; Random Number Seed From Clock (*seed* is global)
               (random-next)                      ; Next Random Number Using Seed
               (plug-in-diffraction 1 image fond .815 1.221 1.123 (+ .821 (rand 2)) (+ .821 (rand 2)) (+ .974 (rand 2)) .610 .677 .636 .066 (+ 27.126 (rand 20)) (+ -0.437 (rand 1)))              
           ))
 			
-(define (script-fu-layerfx-material-overlay img
+(define (script-fu-layerfx-material-mo-overlay img
 					 drawable
 					 color
 					 opacity
@@ -194,53 +218,57 @@
     (gimp-selection-all img)
     ;(gimp-context-set-foreground color)
 				(if (= color 0) 
-(material-carbon colorlayer)
+(material-mo-carbon colorlayer)
 			
 		)
 		
 		(
     if ( = color 1)
-        (material-alubrushed colorlayer img)
+        (material-mo-alubrushed colorlayer img)
 
 )
 (
     if ( = color 2)
-        (material-alubrushed-light colorlayer img)
+        (material-mo-alubrushed-light colorlayer img)
 
 )
 (
     if ( = color 3)
-        (material-blue-jeans colorlayer img 0)
+        (material-mo-blue-jeans colorlayer img 0)
 
 )
 (
     if ( = color 4)
-        (material-blue-jeans colorlayer img 1)
+        (material-mo-blue-jeans colorlayer img 1)
+
+)
+ (   if ( = color 5)
+        (material-mo-blue-jeans colorlayer img 2)
 
 )
 
-	(if (= color 5) 
-(material-bovination-2 colorlayer img)
-			
-		)
 	(if (= color 6) 
-(material-camo2 colorlayer img)
+(material-mo-bovination-2 colorlayer img))
 			
-		)
+		
 	(if (= color 7) 
-(material-plasma colorlayer img)
+(material-mo-camo2 colorlayer img))
 			
-		)
+		
 	(if (= color 8) 
-(material-patchwork colorlayer img)
+(material-mo-plasma colorlayer img)
 			
 		)
-		(if (= color 9) 
-(material-diffraction colorlayer img)
+	(if (= color 9) 
+(material-mo-patchwork colorlayer img)
 			
 		)
 		(if (= color 10) 
-		(material-pizza colorlayer img)
+(material-mo-diffraction colorlayer img)
+			
+		)
+		(if (= color 11) 
+		(material-mo-pizza colorlayer img)
 		)
     ;(gimp-drawable-edit-fill colorlayer 0)
     (gimp-selection-none img)
@@ -280,7 +308,7 @@
   (gimp-image-undo-group-end img)
 )
 	
-(script-fu-register "script-fu-layerfx-material-overlay"
+(script-fu-register "script-fu-layerfx-material-mo-overlay"
 		    "Material Overlay..."
 		    "Overlays a material over a layer."
 		    "vitforlinux"
@@ -289,9 +317,9 @@
 		    ""
 		    SF-IMAGE		"Image"			0
 		    SF-DRAWABLE		"Drawable"		0
-		    	SF-OPTION		"Fill with Material"				list-material-dir
+		    	SF-OPTION		"Fill with Material"				list-material-mo-dir
 		    SF-ADJUSTMENT	"Opacity"		'(100 0 100 1 10 1 0)
 		    SF-OPTION		"Blending Mode"		'("Normal" "Dissolve" "Multiply" "Divide" "Screen" "Overlay" "Dodge" "Burn" "Hard Light" "Soft Light" "Grain Extract" "Grain Merge" "Difference" "Addition" "Subtract" "Darken Only" "Lighten Only" "Hue" "Saturation" "Color" "Value")
 		    SF-TOGGLE		"Merge with layer"	FALSE)
-(script-fu-menu-register "script-fu-layerfx-material-overlay" "<Image>/Layer/Layer Effects By Jon Stipe 299/")
-;(script-fu-menu-register "script-fu-layerfx-material-overlay" "<Layers>/Layer Effects By Jon Stipe 299/")
+(script-fu-menu-register "script-fu-layerfx-material-mo-overlay" "<Image>/Layer/Layer Effects By Jon Stipe 299/")
+;(script-fu-menu-register "script-fu-layerfx-material-mo-overlay" "<Layers>/Layer Effects By Jon Stipe 299/")
