27d7ac23f128de4563d8486512d3e3de70aad5cc
vitforlinux <127871421+vitforlinux-gimp@users.noreply.github.com>
2024-09-20 16:46:51 +0200

    changed font now OFL

--- Snow Cover299.scm
+++ Snow Cover299.scm
@@ -355,7 +355,7 @@
   ""
   SF-TEXT       "Text"    "SEASONS\nGREETINGS"
   SF-COLOR      "Text color"         '(255 0 0)
-  SF-FONT       "Font"               "Arial Bold"
+  SF-FONT       "Font"               "QTChanceryType-Bold"
   SF-ADJUSTMENT "Font size (pixels)" '(250 6 500 1 1 0 1)
   		                        SF-OPTION     _"Text Justification"    '("Centered" "Left" "Right" "Fill") 
 				    SF-ADJUSTMENT _"Letter Spacing"        '(0 -50 50 1 5 0 1)
