f8d4a3c8482360279dc4df993a17f4e4dec4846c
vitforlinux <127871421+vitforlinux-gimp@users.noreply.github.com>
2024-12-23 13:48:00 +0100

    works in 2.10 & 2.99.19(latest) no 3.0rc1

--- highlighted-logo-300.scm
+++ highlighted-logo-300.scm
@@ -6,16 +6,25 @@
         (define sffont "QTVagaRound Bold")
   (define sffont "QTVagaRound-Bold"))
   
-  		(define (remove-color img  fond color)(begin
-		;(plug-in-colortoalpha RUN-NONINTERACTIVE image highlight '(128 128 128))
-		(gimp-image-select-color img 2 fond color)
-		(gimp-drawable-edit-fill fond FILL-TRANSPARENT)
-		(gimp-selection-none img)
-		))
+  		(define (remove-color2 img  fond color)(begin
+		 (cond((not(defined? 'plug-in-colortoalpha))
+		 		     (gimp-drawable-merge-new-filter fond "gegl:color-to-alpha" 0 LAYER-MODE-REPLACE 1.0
+"color" color "transparency-threshold" 0 "opacity-threshold" 1)
+		)
+		(else
+(plug-in-colortoalpha RUN-NONINTERACTIVE img fond color)
+		))))
   
-  		(define (apply-gauss img drawable x y)(begin (if (= (string->number (substring (car(gimp-version)) 0 3)) 2.10)
-      (plug-in-gauss  1  img drawable x y 0)
- (plug-in-gauss  1  img drawable (* x 0.32) (* y 0.32) 0)  )))
+  		(define (apply-gauss2 img drawable x y)
+       (cond ((not(defined? 'plug-in-gauss))
+           (gimp-drawable-merge-new-filter drawable "gegl:gaussian-blur" 0 LAYER-MODE-REPLACE 1.0
+                                    "std-dev-x" (* x 0.32) "std-dev-y" (* y 0.32) "filter" "auto")
+      )
+       (else
+	(plug-in-gauss 1 img drawable x y 0)
+))
+
+ )
   
   (script-fu-register
             "script-fu-highlighted-logo-300"                        ;function name
@@ -93,6 +102,8 @@ SF-ADJUSTMENT _"Outline"          '(0 0 20 1 10 0 0);a spin-button
 						       ((= justification 3) 3)))
       ) ;end of our local variables
       (gimp-image-insert-layer theImage theLayer 0 0)
+	(gimp-context-push)
+	(gimp-context-set-paint-mode 0)
       (gimp-context-set-background bgcolor )
       (gimp-context-set-foreground inTextColor)
       (gimp-drawable-fill theLayer FILL-BACKGROUND)
@@ -189,15 +200,27 @@ SF-ADJUSTMENT _"Outline"          '(0 0 20 1 10 0 0);a spin-button
 		;(gimp-layer-resize-to-image-size highlight-channel)
 
 	(gimp-selection-none theImage)
-	(apply-gauss theImage highlight 5 5)
+	(apply-gauss2 theImage highlight 5 5)
 	;(gimp-image-set-active-layer image highlight)
-	(plug-in-bump-map RUN-NONINTERACTIVE theImage highlight highlight-channel 135 15 10 0 0 0 1 TRUE FALSE 0) ;{LINEAR(0),SPHERICAL(1),SINUSOIDAL(2)}
+	;
+	(cond((not(defined? 'plug-in-bump-map))
+	    (let* ((filter (car (gimp-drawable-filter-new highlight "gegl:bump-map" ""))))
+      (gimp-drawable-filter-configure filter LAYER-MODE-REPLACE 1.0
+                                      "azimuth" 135 "elevation" 15 "depth" 10
+                                      "offset-x" 0 "offset-y" 0 "waterlevel" 0.0 "ambient" 1.0
+                                      "compensate" TRUE "invert" FALSE "type" "linear"
+                                      "tiled" FALSE)
+      (gimp-drawable-filter-set-aux-input filter "aux" highlight-channel)
+      (gimp-drawable-merge-filter highlight filter)
+    ))
+    (else
+    (plug-in-bump-map RUN-NONINTERACTIVE theImage highlight highlight-channel 135 15 10 0 0 0 1 TRUE FALSE 0))) ;{LINEAR(0),SPHERICAL(1),SINUSOIDAL(2)}
 	;(plug-in-colortoalpha RUN-NONINTERACTIVE theImage highlight '(128 128 128))
-	(remove-color theImage highlight '(128 128 128))
-		(apply-gauss theImage highlight 1.5 1.5)
+	(remove-color2 theImage highlight '(128 128 128))
+		;(apply-gauss2 theImage highlight 1.5 1.5)
 		;(gimp-image-remove-layer theImage highlight-channel)
 
-      
+      (gimp-context-pop)
       (gimp-display-new theImage)
       (list theImage theLayer theText)
     )
